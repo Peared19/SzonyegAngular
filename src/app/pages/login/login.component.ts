e4ac3ecbf75f60dde56da23b3e96e3ca5ec3ee79
Bitó Marcell <h347920@stud.u-szeged.hu>
2025-04-08 12:47:08 +0200

    navbar fully done

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import {MatFormField} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    MatFormField,MatFormFieldModule, MatInputModule, MatSelectModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -2,20 +2,31 @@ import {AfterViewInit, Component, EventEmitter, OnInit, Output} from '@angular/c
 import {RouterLink} from '@angular/router';
 import {AppComponent} from '../../app.component';
 import {HomeComponent} from '../../pages/home/home.component';
+import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import {MatLine} from '@angular/material/core';
 
 @Component({
   selector: 'app-menu',
-  imports: [
-    RouterLink, HomeComponent,MatButtonModule,MatIconModule
-  ],
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.scss'
+  styleUrl: './menu.component.scss',
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatLine
+  ]
+
 })
 export class MenuComponent{
   @Output()  selectedPage: EventEmitter<string> = new EventEmitter();
   isMenuOpen = false;
+  currentPage='home';
 
 
   constructor() {
@@ -23,7 +34,9 @@ export class MenuComponent{
   }
 
   setPage(pagename: string) {
+    this.currentPage = pagename;
     this.selectedPage.emit(pagename);
+
     this.isMenuOpen=false;
   }
 }
