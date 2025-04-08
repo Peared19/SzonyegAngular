b45df0b4d9f0fa3c09ec16689a62632adf4fd505
Bitó Marcell <h347920@stud.u-szeged.hu>
2025-04-08 12:04:53 +0200

    navbar looks lowkey good

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -2,12 +2,13 @@ import {AfterViewInit, Component, EventEmitter, OnInit, Output} from '@angular/c
 import {RouterLink} from '@angular/router';
 import {AppComponent} from '../../app.component';
 import {HomeComponent} from '../../pages/home/home.component';
-import {MatAnchor, MatButton,MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-menu',
   imports: [
-    RouterLink, HomeComponent, MatAnchor, MatAnchor,MatButtonModule
+    RouterLink, HomeComponent,MatButtonModule,MatIconModule
   ],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
@@ -23,5 +24,6 @@ export class MenuComponent{
 
   setPage(pagename: string) {
     this.selectedPage.emit(pagename);
+    this.isMenuOpen=false;
   }
 }
